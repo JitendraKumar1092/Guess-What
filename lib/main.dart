576315d63082bdd2992bb1f462bada6639a45176
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2022-11-22 14:01:36 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:guesswhat/HomePage.dart';
+// import 'package:guesswhat/widgets/PaintScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
